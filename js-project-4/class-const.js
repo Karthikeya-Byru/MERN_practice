651edd9e1d9c6f27b2acbdca5d6784fd3599bb47
Karthikeya-Byru <karthikeyanbyru@gmail.com>
2025-05-23 15:29:20 +0530

    updated_logic

--- js-project-4/class-const.js
+++ js-project-4/class-const.js
@@ -7,11 +7,13 @@ function Student(name,age,marks){
 //add method to student type
 Student.prototype.getAverage=function(){
     //calculate avg marks
-
+    let avg = (this.marks[0]+this.marks[1]+this.marks[2])/3
+    return avg;
 }
 
 let std=new Student('raju',20,[97,85,91])
 console.log(std);
+console.log(std.getAverage());
 
 //class 
 class Student1{
@@ -23,7 +25,10 @@ class Student1{
 
     getAverage(){
         //business logic
+        let avg = (this.marks[0]+this.marks[1]+this.marks[2])/3
+        return avg;
     }
 }
 let std1=new Student1('raju',20,[97,85,91])
 console.log(std1);
+console.log(std1.getAverage());
