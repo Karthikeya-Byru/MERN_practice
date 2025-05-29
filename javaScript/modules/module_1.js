9e486f6cbccef10939be7f588bbf1fa1d9eff93f
Karthikeya-Byru <karthikeyanbyru@gmail.com>
2025-05-29 23:33:47 +0530

    sum

---
+++ javaScript/modules/module_1.js
@@ -0,0 +1,6 @@
+//named export
+export let a = 110;
+export let student={
+    name:'revi',
+    class: 8
+}
---
+++ javaScript/modules/module_2.js
@@ -0,0 +1,4 @@
+//import of named export
+import { a , student } from "./module_1.js";
+console.log(a);
+console.log(student.name);
