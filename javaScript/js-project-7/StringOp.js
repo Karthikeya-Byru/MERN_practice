140c0f86fd47784dd308634c7343b34f1a69de68
Karthikeya-Byru <karthikeyanbyru@gmail.com>
2025-05-29 11:35:19 +0530

    operations on string

---
+++ javaScript/js-project-7/StringOp.js
@@ -0,0 +1,20 @@
+let fullName='Byru Karthikeyan';
+//length
+console.log(fullName.length);
+//charat
+console.log(fullName.charAt(5));
+//slice
+console.log(fullName.slice(0,4));
+//substring
+console.log(fullName.substring(0,4));
+//replace
+console.log(fullName.replace('yru','airu'));
+//replaceall this method replaces all the elements 
+console.log(fullName.replaceAll('a','n'));
+//touppercase and to lowercase
+console.log(fullName.toUpperCase());
+//concatenation
+console.log(fullName.concat(' is a good boy'));
+//covert string to an array
+//split method
+console.log(fullName.split(' '));
