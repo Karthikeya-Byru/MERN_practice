0fc1f1077966c977b31318eb54609ca64712cbec
Karthikeya-Byru <karthikeyanbyru@gmail.com>
2025-05-31 13:45:32 +0530

    expnimp

--- javaScript/modules/module_1.js
+++ javaScript/modules/module_1.js
@@ -1,6 +1,17 @@
 //named export
 export let a = 110;
-export let student={
+let student={
     name:'revi',
     class: 8
 }
+//shortcut for export
+export{student}
+
+//default export
+let b =10;
+let name = 'ravi';
+let point={
+    x:2,
+    y:-2
+}
+export default {b,name,point}
--- javaScript/modules/module_2.js
+++ javaScript/modules/module_2.js
@@ -2,3 +2,8 @@
 import { a , student } from "./module_1.js";
 console.log(a);
 console.log(student.name);
+
+//import from default export
+import x from "./module_1.js";
+console.log(x);
+console.log(x.point);
